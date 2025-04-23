6cee5f8196d776b74e9d92346eb31c58c187f202
Amani Khalid <amanikhalidd@gmail.com>
2025-04-23 08:51:22 +0400

    Adding main structure

--- Program.cs
+++ Program.cs
@@ -2,9 +2,59 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //constants
+        const double MinimumBalance = 100.0;
+        const string AccountsFilePath = "accounts.txt";
+        const string ReviewFilePath = "reviews.txt";
+
+        //Global lists (parallel)
+        static List<int> accountNumbers = new List<int>();
+        static List<string> accountNames = new List<string>();
+        static List<double> accountBalances = new List<double>();
+
+        //static List<Queue<string>> transactions = new List<Queue<string>>();
+        // Queues and Stacks
+        //static Queue<(string name, string nationalID)> createAccountRequests = new Queue<(string name, string nationalID)>();
+        static Queue<string> createAccountRequests = new Queue<string>(); //format: "Name|NationalID"
+        static Stack<string> reviewStack = new Stack<string>();
+
+        // Account number generator
+        static int lastAccountNumber;
+        static void Main()
         {
-            
+            LoadAccountsInformationFromFile();
+            LoadReviews();
+
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("\nWelcome to the SafeBank System");
+                Console.WriteLine("1. User Menu");
+                Console.WriteLine("2. Admin Menu");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Select option: ");
+
+                string mainChoice = Console.ReadLine();
+
+                switch (mainChoice)
+                {
+                    case "1":
+                        UserMenu();
+                        break;
+                    case "2":
+                        AdminMenu();
+                        break;
+                    case "0":
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Thank you for using the SafeBank System. Goodbye!");
         }
     }
 }
