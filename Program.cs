5ffdc6d6c4620ecfe711fa533bed899d5ce0cb2d
Amani Khalid <amanikhalidd@gmail.com>
2025-07-02 14:39:36 +0400

    Added List to store feedback ratings

--- Program.cs
+++ Program.cs
@@ -73,6 +73,7 @@ namespace MiniBankSystem
         static List<bool> hasActiveLoan = new List<bool>(); // List to track if the account has an active loan
         static List<double> loanAmounts = new List<double>(); // List to store loan amounts
         static List<double> loanInterestRate = new List<double>(); // List to store loan interest rates
+        static List<int> feedbackRatings = new List<int>(); // List to store feedback ratings
 
         // Account number generator
         static int lastAccountNumber;
